6e63e2a4f90a59b6a172525f6620f1ac461506c0
Steve Boyer <stephen.a.boyer@gmail.com>
2022-08-05 10:53:33 -0500

    Add styles to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css"
 import { BrowserRouter, Route } from "react-router-dom"
+import { makeStyles } from "@material-ui/core"
 import Header from "./components/Header"
 import Home from "./pages/Home"
 import Coin from "./pages/Coin"
 
+
+const useStyles = makeStyles(() => ({
+  App: {
+    minHeight: "100vh",
+  },
+}))
+
+
 function App() {
+
+  const classes = useStyles()
 
   return (
     <BrowserRouter>
-      <div>
+      <div className={classes.App}>
         <Header />
         <Route path='/' component={Home} />
         <Route path='/coins/:id' component={Coin} />
@@ -17,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
