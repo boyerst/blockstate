166a68abd40fc08c25a748463dbb48530cf937ca
Steve Boyer <stephen.a.boyer@gmail.com>
2022-08-05 11:24:58 -0500

    Add darkMode and darkMode Switch

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
-import "./App.css"
 import { BrowserRouter, Route } from "react-router-dom"
-import { makeStyles } from "@material-ui/core"
+import { makeStyles, createTheme, ThemeProvider, Switch, Paper } from "@material-ui/core"
+import { useState } from 'react';
 import Header from "./components/Header"
 import Home from "./pages/Home"
 import Coin from "./pages/Coin"
 
 
-const useStyles = makeStyles(() => ({
-  App: {
-    minHeight: "100vh",
-  },
-}))
+// const useStyles = makeStyles(() => ({
+//   App: {
+//     minHeight: "100vh",
+//   },
+// }))
 
 
 function App() {
 
-  const classes = useStyles()
+  // const classes = useStyles()
+
+  const [darkMode, setDarkMode] = useState(false)
+
+  const theme = createTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+    },
+  });
+
+
+
 
   return (
     <BrowserRouter>
-      <div className={classes.App}>
-        <Header />
-        <Route path='/' component={Home} />
-        <Route path='/coins/:id' component={Coin} />
-      </div>
+      <ThemeProvider theme={theme}>
+        <Paper style={{ height: '100vh' }}>
+          <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)}/>
+          <div >
+            <Header />
+            <Route path='/' component={Home} />
+            <Route path='/coins/:id' component={Coin} />
+          </div>
+        </Paper>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
