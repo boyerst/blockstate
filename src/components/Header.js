db076d6b78ce9d02856267fc2c9f8a0c5d1bd073
Steve Boyer <stephen.a.boyer@gmail.com>
2022-08-06 15:16:53 -0500

    Style Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,29 @@
-import { AppBar, Container, Toolbar, Typography, Select, MenuItem } from "@material-ui/core"
+import { makeStyles, AppBar, Container, Toolbar, Typography, Select, MenuItem } from "@material-ui/core"
+
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1, 
+    color: "blue",
+    fontFamily: "Open Sans",
+    fontWeight: "bold",
+    cursor: "pointer"
+  }
+  
+}))
+
+
 
 
 const Header = () => {
+
+  const classes = useStyles()
+
   return(
     <AppBar color='transparent' position='static' >
       <Container>
         <Toolbar>
-          <Typography>
+          <Typography className={classes.title}>
             BlockState
           </Typography>
           <Select 
