baa3c13e91be2ae7c8a59fd518f9acea67aed92c
Steve Boyer <stephen.a.boyer@gmail.com>
2022-08-08 09:38:32 -0500

    Create and set 'Currency' state

--- src/CurrencyContext.js
+++ src/CurrencyContext.js
@@ -1,9 +1,19 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 
 
 const Currency = createContext()
 
 const CurrencyContext = ({children}) => {
+
+  const [currency, setCurrency] = useState("USD")
+  const [symbol, setSymbol] = useState("$")
+
+  useEffect(() => {
+    if (currency === "USD") setSymbol("$")
+    else if (currency === "BTC") setSymbol("₿")
+  })
+
+
   return(
     <Currency.Provider>
       {children}
