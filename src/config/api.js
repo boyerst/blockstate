0713b6dd47890f15635179970ca6b987d68b42a2
Steve Boyer <stephen.a.boyer@gmail.com>
2022-08-10 11:36:25 -0500

    Add API endpoint for data from each coins' contract address

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,12 @@
 
 
-export const CoinMarketData = (currency) =>
+export const CoinsMarketData = (currency) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+
+
+
+export const CoinContractData = (id) =>
+  `https://api.coingecko.com/api/v3/coins/${id}`;
+
+
+
