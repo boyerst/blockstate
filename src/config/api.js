27e0b2b337153ecd7dd6966e080bdb3b3fa93131
Steve Boyer <stephen.a.boyer@gmail.com>
2022-08-10 11:39:37 -0500

    Rename previous two API endpoints and add API endpoint for historic coin data

--- src/config/api.js
+++ src/config/api.js
@@ -1,12 +1,14 @@
 
 
-export const CoinsMarketData = (currency) =>
+export const AllCoinsMarketData = (currency) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
 
 
 
-export const CoinContractData = (id) =>
+export const CoinMarketData = (id) =>
   `https://api.coingecko.com/api/v3/coins/${id}`;
 
 
 
+export const CoinHistoricData = (id, days = 365, currency) =>
+  `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
