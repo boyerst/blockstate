08ac5fe054bd99dbc7ba8637da7a3ce169b6b092
Steve Boyer <stephen.a.boyer@gmail.com>
2022-08-11 12:55:50 -0500

    Style Carousel container

--- src/components/Banner/Carousel.js
+++ src/components/Banner/Carousel.js
@@ -1,8 +1,19 @@
+import { makeStyles } from "@material-ui/core"
 
+const useStyles = makeStyles(() => ({
+  carousel: {
+    height: "50%",
+    display: "flex",
+    alignItems: "center"
+  }
+}))
 
 const Carousel = () => {
+
+  const classes = useStyles()
+
   return (
-    <div>
+    <div className={classes.carousel}>
       Carousel
     </div>
 
