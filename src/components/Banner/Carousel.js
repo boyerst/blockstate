cc58be2578ca06619a497503de302a22f2e0fc48
Steve Boyer <stephen.a.boyer@gmail.com>
2022-08-14 00:47:48 -0500

    Add fetch trending coins function

--- src/components/Banner/Carousel.js
+++ src/components/Banner/Carousel.js
@@ -1,6 +1,7 @@
 import { makeStyles } from "@material-ui/core"
 import axios from "axios"
 import { CurrencyState } from "../../CurrencyContext"
+import { TrendingCoins } from "../../config/api"
 
 
 const useStyles = makeStyles(() => ({
@@ -16,6 +17,13 @@ const Carousel = () => {
   const classes = useStyles()
 
   const { currency, setCurrency } = CurrencyState()
+
+  const fetchTrendingCoins = async () => {
+    const { data } = await axios.get(TrendingCoins(currency))
+    
+  }
+
+
 
   return (
     <div className={classes.carousel}>
