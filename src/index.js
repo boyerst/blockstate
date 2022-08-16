860c5e5c5625f44197ddf324ed36e665c284b06d
Steve Boyer <stephen.a.boyer@gmail.com>
2022-08-16 11:51:17 -0500

    Import react-alice-carousel style into index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import CurrencyContext from "./CurrencyContext"
+import 'react-alice-carousel/lib/alice-carousel.css'
 
 
 
