d6d932323ec4ccc12a5200a8f892855118380e05
Steve Boyer <stephen.a.boyer@gmail.com>
2022-08-19 10:59:27 -0500

    Fetch AllCoinsMarketData API call

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -1,8 +1,14 @@
-
+import axios from "axios"
+import { AllCoinsMarketData } from "../config/api"
 
 
 const CoinsTable = () => {
   
+  const fetchAllCoinsMarketData = async () => {
+    const { data } = await axios.get(AllCoinsMarketData())   
+  }
+
+
   return (
     <div>
       CoinsTable
