1959f9dfa84740ae7596aad7ab2fb75bba80fa43
Steve Boyer <stephen.a.boyer@gmail.com>
2022-08-19 11:01:18 -0500

    Fetch and pass in currency context to API fetch

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -1,12 +1,16 @@
 import axios from "axios"
 import { AllCoinsMarketData } from "../config/api"
+import { CurrencyState } from "../CurrencyContext"
 
 
 const CoinsTable = () => {
   
+  const { currency } = CurrencyState()
+
   const fetchAllCoinsMarketData = async () => {
-    const { data } = await axios.get(AllCoinsMarketData())   
+    const { data } = await axios.get(AllCoinsMarketData(currency))   
   }
+
 
 
   return (
