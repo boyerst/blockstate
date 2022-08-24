1b6b5a04ac1b942256f63f430062010bd1699d49
Steve Boyer <stephen.a.boyer@gmail.com>
2022-08-24 16:03:09 -0500

    Add state for pagination and slice out 10 coins per page for map() function

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -39,6 +39,7 @@ const CoinsTable = () => {
   const [coins, setCoins] = useState([])
   const [loading, setLoading] = useState(false)
   const [search, setSearch] = useState("")
+  const [page, setPage] = useState(1)
 
   const history = useHistory()
   const { currency, symbol } = CurrencyState()
@@ -108,68 +109,70 @@ const CoinsTable = () => {
             </TableHead>
             <TableBody>
             {
-              handleSearch().map((row) => {
-                const profit = row.price_change_percentage_24h > 0
-                return (
-                  <TableRow
-                    key={row.name}
-                    onClick={() => history.push(`/coins/${row.id}`)}
-                    className={classes.row}
-                  >
-                    <TableCell
-                      component="th"
-                      scope="row"
-                      style={{
-                        display: "flex",
-                        gap: 15
-                      }}
+              handleSearch()
+                .slice((page - 1) * 10, (page - 1) * 10 + 10)
+                .map((row) => {
+                  const profit = row.price_change_percentage_24h > 0
+                  return (
+                    <TableRow
+                      key={row.name}
+                      onClick={() => history.push(`/coins/${row.id}`)}
+                      className={classes.row}
                     >
-                      <img 
-                        src={row?.image}
-                        alt={row.name}
-                        height="50"
-                        style={{ marginBottom: 6 }}
-                      />
-                      <div
-                        style={{ display: "flex", flexDirection: "column" }}
+                      <TableCell
+                        component="th"
+                        scope="row"
+                        style={{
+                          display: "flex",
+                          gap: 15
+                        }}
                       >
-                        <span
-                          style={{
-                            textTransform: "uppercase",
-                            fontSize: 22
-                          }}
+                        <img 
+                          src={row?.image}
+                          alt={row.name}
+                          height="50"
+                          style={{ marginBottom: 6 }}
+                        />
+                        <div
+                          style={{ display: "flex", flexDirection: "column" }}
                         >
-                          {row.symbol}
-                        </span>
-                        <span
-                          style={{
-                            color: "darkgrey"
-                          }}
-                        >
-                          {row.name}
-                        </span>
-                      </div>
-                    </TableCell>
-                    <TableCell align="right">
-                      {symbol}{" "}
-                      {numberWithCommas(row.current_price.toFixed(2))}
-                    </TableCell>
-                    <TableCell 
-                      align="right"
-                      style={{
-                        color: profit > 0 ? "rgb(14, 203, 129)"  : "red",
-                        fontWeight: 600
-                      }}
-                    >
-                      {profit && "+"}
-                      {row.price_change_percentage_24h}
-                    </TableCell>
-                    <TableCell align="right">
-                      {symbol}{" "}
-                      {numberWithCommas(row.market_cap.toString())}
-                    </TableCell>
-                  </TableRow>
-                )    
+                          <span
+                            style={{
+                              textTransform: "uppercase",
+                              fontSize: 22
+                            }}
+                          >
+                            {row.symbol}
+                          </span>
+                          <span
+                            style={{
+                              color: "darkgrey"
+                            }}
+                          >
+                            {row.name}
+                          </span>
+                        </div>
+                      </TableCell>
+                      <TableCell align="right">
+                        {symbol}{" "}
+                        {numberWithCommas(row.current_price.toFixed(2))}
+                      </TableCell>
+                      <TableCell 
+                        align="right"
+                        style={{
+                          color: profit > 0 ? "rgb(14, 203, 129)"  : "red",
+                          fontWeight: 600
+                        }}
+                      >
+                        {profit && "+"}
+                        {row.price_change_percentage_24h}
+                      </TableCell>
+                      <TableCell align="right">
+                        {symbol}{" "}
+                        {numberWithCommas(row.market_cap.toString())}
+                      </TableCell>
+                    </TableRow>
+                  )    
               })
             }
           
