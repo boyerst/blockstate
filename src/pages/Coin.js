a5041effffd0e44e4587e1e0a4676d87335c9ed5
Steve Boyer <stephen.a.boyer@gmail.com>
2022-08-26 12:37:51 -0500

    Bring CurrencyContext into Coin.js

--- src/pages/Coin.js
+++ src/pages/Coin.js
@@ -1,7 +1,15 @@
 import "../App.css"
+import { useParams } from "react-router-dom"
+import { useState } from 'react'
+import { CurrencyState } from "../CurrencyContext"
 
 
 const Coin = () => {
+
+  const { id } = useParams()
+  const [coin, setCoin] = useState()
+  const { currency, symbol } = CurrencyState()
+
   return(
     <div>
       Coin 
