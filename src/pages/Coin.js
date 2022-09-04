4fa439257dd0e9b2e9ad72b619d782671f8e83d2
Steve Boyer <stephen.a.boyer@gmail.com>
2022-09-03 20:30:52 -0500

    Re-order coin market data

--- src/pages/Coin.js
+++ src/pages/Coin.js
@@ -114,6 +114,15 @@ const Coin = () => {
           </span>
           <span style ={{ display: "flex" }}>
             <Typography variant="h6" className={classes.heading}>
+              Market Cap: 
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography variant="h6" style={{ fontFamily: "Open Sans" }}>
+              {symbol}{" "}{numberWithCommas(coin?.market_data.market_cap[currency.toLowerCase()])}
+            </Typography>
+          </span>
+          <span style ={{ display: "flex" }}>
+            <Typography variant="h6" className={classes.heading}>
               Current Price: 
             </Typography>
             &nbsp; &nbsp;
@@ -127,15 +136,6 @@ const Coin = () => {
           </span>
           <span style ={{ display: "flex" }}>
             <Typography variant="h6" className={classes.heading}>
-              Market Cap: 
-            </Typography>
-            &nbsp; &nbsp;
-            <Typography variant="h6" style={{ fontFamily: "Open Sans" }}>
-              {symbol}{" "}{numberWithCommas(coin?.market_data.market_cap[currency.toLowerCase()])}
-            </Typography>
-          </span>
-          <span style ={{ display: "flex" }}>
-            <Typography variant="h6" className={classes.heading}>
               Circulating Supply:
             </Typography>
             &nbsp; &nbsp;
@@ -145,11 +145,11 @@ const Coin = () => {
           </span>
           <span style ={{ display: "flex" }}>
             <Typography variant="h6" className={classes.heading}>
-              { coin?.market_data.max_supply ? "Max Supply" : "Total Supply"}:
+              {coin?.market_data.max_supply ? "Max Supply" : "Total Supply"}:
             </Typography>
             &nbsp; &nbsp;
             <Typography variant="h6" style={{ fontFamily: "Open Sans" }}>
-              { numberWithCommas(coin?.market_data.max_supply || coin?.market_data.total_supply.toFixed(2)) }
+              {numberWithCommas(coin?.market_data.max_supply || coin?.market_data.total_supply.toFixed(2))}
             </Typography>
           </span>
           <span style ={{ display: "flex" }}>
