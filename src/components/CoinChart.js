5e9a891a112f83bb300875d9012d6cd3df18c324
Steve Boyer <stephen.a.boyer@gmail.com>
2022-09-04 14:46:02 -0500

    Add state to CoinChart.js

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -1,6 +1,11 @@
+import { useState } from "react"
 
 
 const CoinChart = () => {
+
+  const [historicalData, setHistoricalData] = useState()
+  const [days, setDays] = useState(1)
+
   return (
     <div>
       CoinChart
