30ce3a7f74cb7d711b33d8dfbdd455a64c70696b
Steve Boyer <stephen.a.boyer@gmail.com>
2022-09-04 14:47:01 -0500

    Add currency context to CoinChart.js

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -1,10 +1,13 @@
 import { useState } from "react"
+import { CurrencyState } from "../CurrencyContext"
 
 
 const CoinChart = () => {
 
   const [historicalData, setHistoricalData] = useState()
   const [days, setDays] = useState(1)
+
+  const { currency, symbol } = CurrencyState()
 
   return (
     <div>
