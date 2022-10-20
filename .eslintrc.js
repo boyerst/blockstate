84203960cce5fc5557ae50bbf755fb149347e5bd
Steve Boyer <stephen.a.boyer@gmail.com>
2022-10-20 19:38:11 -0400

    Install and config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+
+// const a11yOff = Object.keys(require('eslint-plugin-jsx-a11y').rules)
+//   .reduce((acc, rule) => { acc[`jsx-a11y/${rule}`] = 'off'; return acc }, {})
+
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    // ...a11yOff,
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/jsx-wrap-multilines": "off",
+    "react/prop-types": 0,
+    "react/jsx-props-no-spreading": 0,
+    "quotes": [0, "double"],
+    "quote-props": "off",
+    "semi": [0, "never"],
+    "padded-blocks": ["error", { "classes": "always" }],
+    "react/destructuring-assignment": "off",
+    "comma-dangle": "off",
+    "operator-linebreak": "off",
+    "no-multiple-empty-lines": "off"
+  },
+};
