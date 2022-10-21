236d0fb70c2f1f96fbd7d1fb482067be8042c810
Steve Boyer <stephen.a.boyer@gmail.com>
2022-10-20 20:50:24 -0400

    Lint api.js

--- src/config/api.js
+++ src/config/api.js
@@ -8,13 +8,13 @@ export const CoinMarketData = (id) =>
   `https://api.coingecko.com/api/v3/coins/${id}`;
 
 
-export const CoinHistoricData = (id, days = 365, currency) =>
+export const CoinHistoricData = (id, currency, days = 365) =>
   `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
 
 
 export const TrendingCoins = (currency) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
 
-export const GlobalMarketData = () => 
+export const GlobalMarketData = () =>
   `https://api.coingecko.com/api/v3/global`;
 
