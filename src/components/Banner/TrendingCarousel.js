78de393b7d78f81651cc53cc158ef600cf15adfa
Steve Boyer <stephen.a.boyer@gmail.com>
2022-10-25 13:22:19 -0400

    Fetch and setState with trending coins

--- src/components/Banner/TrendingCarousel.js
+++ src/components/Banner/TrendingCarousel.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import { TrendingCoins } from "../../config/api"
 import axios from "axios"
 import { makeStyles } from "@material-ui/core"
 
@@ -18,8 +19,24 @@ function TrendingCarousel() {
   const classes = useStyles()
 
 
+  const fetchTrendingCoins = async () => {
+    const { data } = await axios.get(TrendingCoins())
+    const { coins, exchanges } = data
+    setTrending(coins)
+  }
+
+
+  useEffect(() => {
+    fetchTrendingCoins()
+  }, [])
+
+  console.log("🟢trending: ", trending)
+
+
   return (
     <span>Trending Coins</span>
+
+
 
   )
 
