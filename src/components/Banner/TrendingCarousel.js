77d5ce9963bd4e2ffb4b0e484083eb1da6e9e058
Steve Boyer <stephen.a.boyer@gmail.com>
2022-10-25 14:12:16 -0400

    🔥Render trending coins on a carousel

--- src/components/Banner/TrendingCarousel.js
+++ src/components/Banner/TrendingCarousel.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from "react"
-import { TrendingCoins } from "../../config/api"
-import axios from "axios"
+import { Link } from "react-router-dom"
 import { makeStyles } from "@material-ui/core"
+import AliceCarousel from "react-alice-carousel"
+import axios from "axios"
+import { TrendingCoins } from "../../config/api"
 
 
 
 
 const useStyles = makeStyles(() => ({
-
+  carousel: {
+    height: "20%",
+    display: "flex",
+    alignItems: "center",
+    padding: 40
+  },
+  carouselItem: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    textTransform: "uppercase"
+  }
 }))
 
 
@@ -31,14 +44,50 @@ function TrendingCarousel() {
   }, [])
 
   console.log("🟢trending: ", trending)
+  // console.log("🟢trending[0]: ", trending[0].item.id)
+
+  const items = trending.map((coin) => {
+
+    return (
+      <Link
+        to={`/coins/${coin.item.id}`}
+        className={classes.carouselItem}
+      >
+        <span>
+          {coin.item.symbol}
+        </span>
+      </Link>
+    )
+  })
+
+
+  const responsive = {
+    0: {
+      items: 2,
+    },
+    512: {
+      items: 4,
+    }
+  }
 
 
   return (
-    <span>Trending Coins</span>
-
-
+    <div className={classes.carousel}>
+      <AliceCarousel
+        mouseTracking
+        infinite
+        autoPlay
+        autoPlayInterval={1000}
+        animationDuration={1500}
+        disableDotsControls
+        disableButtonsControls
+        responsive={responsive}
+        items={items}
+      />
+    </div>
 
   )
+
 
 }
 
