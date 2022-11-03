13c93df923177a26b69395e7b7505a07f81f0588
Steve Boyer <stephen.a.boyer@gmail.com>
2022-11-03 16:35:08 -0400

    Clean up Banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,6 +1,6 @@
 // import React from "react"
 import "../../App.css"
-import { makeStyles, Container, Typography } from "@material-ui/core"
+import { makeStyles, Container, Typography, Box } from "@material-ui/core"
 import TopCarousel from "./TopCarousel"
 import TrendingCarousel from "./TrendingCarousel"
 
@@ -8,20 +8,14 @@ import TrendingCarousel from "./TrendingCarousel"
 
 
 const useStyles = makeStyles(() => ({
-  bannerContent: {
-    height: 400,
-    display: "flex",
-    flexDirection: "column",
-    paddingTop: 25,
-    paddingBottom: 60,
-    justifyContent: "space-around"
-  },
-  titleType: {
+  container: {
     display: "flex",
     height: "40%",
     flexDirection: "column",
     justifyContent: "center",
-    textAlign: "center"
+    textAlign: "center",
+    paddingTop: 60,
+    paddingBottom: 60
   }
 }))
 
@@ -32,44 +26,33 @@ function Banner() {
 
   return (
     <div>
-      <Container className={classes.bannerContent}>
-        <div className={classes.titleType}>
-{/*          <Typography
-            variant="h2"
-            style={{
-              fontWeight: "bold",
-              marginBottom: 20,
-              fontFamily: "Open Sans"
-            }}
-          >
-            BlockState
-          </Typography>*/}
-          <Typography
-            variant="h6"
-            style={{
-              marginLeft: 20,
-              fontWeight: 700,
-              fontFamily: "Open Sans",
-              textAlign: "left"
-            }}
-          >
-            Trending Coins
-          </Typography>
-          <TrendingCarousel />
-          <Typography
-            variant="h6"
-            style={{
-              marginLeft: 20,
-              fontWeight: 700,
-              fontFamily: "Open Sans",
-              textAlign: "left",
-              paddingTop: 70
-            }}
-          >
-            Top Coins
-          </Typography>
-          <TopCarousel />
-        </div>
+      <Container className={classes.container}>
+        <Typography
+          variant="h6"
+          style={{
+            marginLeft: 20,
+            fontWeight: 700,
+            fontFamily: "Open Sans",
+            textAlign: "left",
+            border: "red"
+          }}
+        >
+          Trending Coins
+        </Typography>
+        <TrendingCarousel />
+        <Typography
+          variant="h6"
+          style={{
+            marginLeft: 20,
+            fontWeight: 700,
+            fontFamily: "Open Sans",
+            textAlign: "left",
+            paddingTop: 70
+          }}
+        >
+          Top Coins
+        </Typography>
+        <TopCarousel />
       </Container>
     </div>
   )
